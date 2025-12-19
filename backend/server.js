da35118e5c672ef6c63c8381d19a83f1b9b6d523
p.lakshmivenkateswara <119067745+lakshmivenkateswara2356@users.noreply.github.com>
2025-12-19 14:11:21 +0530

    Refactor server.js by removing unused imports

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require("fs");
-const path = require("path");
-
-// 🔥 Import DB here
 require("./db");
 
 const app = express();
-const uploadDir = path.join(__dirname, "uploads");
-if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(uploadDir));
 
-// Routes
-app.use("/api/recipe-items", require("./routes/recepi"));
+app.use("/api/recipe-items", require("./routes/recipi"));
 app.use("/api/settings", require("./routes/settingsRoutes"));
 app.use("/api/orders", require("./routes/order"));
 
-// Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
